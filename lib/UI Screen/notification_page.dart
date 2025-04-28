3f6d8bdd1d15cabaf6ff63c7ad4894632ebd8748
shahedroks <shahedroks@gmail.com>
2025-04-28 17:53:59 +0600

    add custom notification field and integrate into notification page

--- lib/UI Screen/notification_page.dart
+++ lib/UI Screen/notification_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../Coustom_Widget/custom_notification_fild.dart';
+
 class NotificationPage extends StatefulWidget {
   const NotificationPage({super.key});
 
@@ -10,6 +12,22 @@ class NotificationPage extends StatefulWidget {
 class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Notifications'), centerTitle: true),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20),
+            CustomNotificationFild(text: "New"),
+            CustomNotificationFild(
+              text: "Earlier",
+              isCheck: true,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
